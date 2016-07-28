5d75409987bcd2038f7f215750caa0d1c75677a7
SRAUFactory <srau.factory@gmail.com>
2016-07-28 14:26:20 +0900

    ダイアログにローディング画像を追加
    EC2情報取得までの間にローディング用の画像を表示するように修正

--- js/get_ec2_info.js
+++ js/get_ec2_info.js
@@ -1,7 +1,7 @@
 function getEc2InstanceHtml(ec2Instance, elementId, elementLabel) {
 	var ec2InfoHtml = '<li>';
 	ec2InfoHtml += '<a data-toggle="collapse" href="#' + elementId + '" aria-expand="false" aria-controls="' + elementId + '">';
-	ec2InfoHtml += elementLabel + '<span class="badge pull-right">+</span></a>';
+	ec2InfoHtml += 'Instance:' + elementLabel + '<span class="badge pull-right">+</span></a>';
 	ec2InfoHtml += '<div class="collapse" id="' + elementId + '">';
 	ec2InfoHtml += '<ul class="nav nav-list nav-list-vivid">';
 	jQuery.each(ec2Instance, function(name, value) {
@@ -21,20 +21,24 @@ function setEc2Info(data) {
 			ec2InfoHtml += getEc2InstanceHtml(this, this.InstanceId, this.InstanceId);
 		});
 	});
-	$($("button#viewEc2Info").attr("data-target") + " .modal-body").html('<ul class="nav nav-list nav-list-vivid">' + ec2InfoHtml  + '</ul>');
+	$("#wrap").html('<ul class="nav nav-list nav-list-vivid">' + ec2InfoHtml  + '</ul>').css('display','block');
+	$('#loader-bg ,#loader').css('display','none');
 }
 
 function getEc2Info() {
+	$('#wrap').css('display','none');
+	$('#loader-bg ,#loader').css('display','block');
 	$.ajax({
 		type : "GET",
 		url : "https://app57971f8a02581.mobingicloud.com/get_ec2_info.php",
 		dataType: 'json',
 		success : setEc2Info
 	});
-	$($("button#viewEc2Info").attr("data-target") + " .modal-body").html("Now loading...");
 }
 
 $(function() {
+	$('#wrap').css('display','none');
+	$('#loader-bg ,#loader').css({'display':'none', 'text-align':'center'});
 	$("div#LinkMenu").css("background-color", "#edeff1");
 	$("button#viewEc2Info").click(getEc2Info);
 });
