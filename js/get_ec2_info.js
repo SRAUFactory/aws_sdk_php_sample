01309a1118685301046717b1e9e55586b30e678a
SRAUFactory <srau.factory@gmail.com>
2016-07-28 14:36:25 +0900

    ダイアログ表示のレイアウト調整

--- js/get_ec2_info.js
+++ js/get_ec2_info.js
@@ -1,14 +1,14 @@
 function getEc2InstanceHtml(ec2Instance, elementId, elementLabel) {
 	var ec2InfoHtml = '<li>';
 	ec2InfoHtml += '<a data-toggle="collapse" href="#' + elementId + '" aria-expand="false" aria-controls="' + elementId + '">';
-	ec2InfoHtml += 'Instance:' + elementLabel + '<span class="badge pull-right">+</span></a>';
+	ec2InfoHtml += elementLabel + '<span class="badge pull-right">+</span></a>';
 	ec2InfoHtml += '<div class="collapse" id="' + elementId + '">';
 	ec2InfoHtml += '<ul class="nav nav-list nav-list-vivid">';
 	jQuery.each(ec2Instance, function(name, value) {
 		if (typeof value == "object") {
 			ec2InfoHtml += getEc2InstanceHtml(value, elementId + name, name);
 		} else {
-			ec2InfoHtml += '<li><a>' + name  + ':' + value + '</a></li>';
+			ec2InfoHtml += '<li><a><dt>' + name  + '</dt><dd>' + value + '</dd></a></li>';
 		}
 	});
 	return ec2InfoHtml + '</ul></div></li>';
@@ -18,11 +18,11 @@ function setEc2Info(data) {
 	var ec2InfoHtml = "";
 	jQuery.each(data, function(){
 		jQuery.each(this.Instances, function() {
-			ec2InfoHtml += getEc2InstanceHtml(this, this.InstanceId, this.InstanceId);
+			ec2InfoHtml += getEc2InstanceHtml(this, this.InstanceId, 'Instance:' + this.InstanceId);
 		});
 	});
-	$("#wrap").html('<ul class="nav nav-list nav-list-vivid">' + ec2InfoHtml  + '</ul>').css('display','block');
 	$('#loader-bg ,#loader').css('display','none');
+	$("#wrap").html('<ul class="nav nav-list nav-list-vivid">' + ec2InfoHtml  + '</ul>').css('display','block');
 }
 
 function getEc2Info() {
